fdc9b9dd19d98a1d9619ab57f431e2b699929c55
CuteBlackCat <854488571@qq.com>
2018-03-20 11:48:01 +0800

    selectTank

--- src/app/canvas/tank/constant.component.ts
+++ src/app/canvas/tank/constant.component.ts
@@ -1,22 +1,22 @@
-export class Constant {
+const Constant =  {
 	/**
 	 * 静态变量
 	*/
 
-	SCREEN_WIDTH = 512; // 屏幕宽
-	SCREEN_HEIGHT = 448; // 屏幕高
+	SCREEN_WIDTH:  512, // 屏幕宽
+	SCREEN_HEIGHT:  448, // 屏幕高
 
 
 	/**************图片资源*****************/
-	MENU_IMAGE = new HTMLImageElement();
-	// MENU_IMAGE.src = 'images/menu.gif';
+	MENU_IMAGE:  new HTMLImageElement(),
+	// MENU_IMAGE.src:  'images/menu.gif',
 	// tslint:disable-next-line:semicolon
-	RESOURCE_IMAGE = new HTMLImageElement();
-	// RESOURCE_IMAGE.src = 'images/tankAll.gif';
+	RESOURCE_IMAGE:  new HTMLImageElement(),
+	// RESOURCE_IMAGE.src:  'images/tankAll.gif',
 
 
 	/**************各个图块在图片中的位置*****************/
-	POS: object = {
+	POS:  {
 		selectTank: [128, 96],
 		stageLevel: [396, 96],
 		num: [256, 96],
@@ -34,47 +34,47 @@ export class Constant {
 		bulletBomb: [320, 0],
 		over: [384, 64],
 		prop: [256, 110]
-	};
+	},
 
 	/**************声音资源*****************/
-	START_AUDIO = new Audio('audio/start.mp3');
-	BULLET_DESTROY_AUDIO = new Audio('audio/bulletCrack.mp3');
-	TANK_DESTROY_AUDIO = new Audio('audio/tankCrack.mp3');
-	PLAYER_DESTROY_AUDIO = new Audio('audio/playerCrack.mp3');
-	MOVE_AUDIO = new Audio('audio/move.mp3');
-	ATTACK_AUDIO = new Audio('audio/attack.mp3');
-	PROP_AUDIO = new Audio('audio/prop.mp3');
+	START_AUDIO:  new Audio('audio/start.mp3'),
+	BULLET_DESTROY_AUDIO:  new Audio('audio/bulletCrack.mp3'),
+	TANK_DESTROY_AUDIO:  new Audio('audio/tankCrack.mp3'),
+	PLAYER_DESTROY_AUDIO:  new Audio('audio/playerCrack.mp3'),
+	MOVE_AUDIO:  new Audio('audio/move.mp3'),
+	ATTACK_AUDIO:  new Audio('audio/attack.mp3'),
+	PROP_AUDIO:  new Audio('audio/prop.mp3'),
 
 
 	/**************游戏状态*****************/
-	GAME_STATE_MENU = 0;
-	GAME_STATE_INIT = 1;
-	GAME_STATE_START = 2;
-	GAME_STATE_OVER = 3;
-	GAME_STATE_WIN = 4;
+	GAME_STATE_MENU:  0,
+	GAME_STATE_INIT:  1,
+	GAME_STATE_START:  2,
+	GAME_STATE_OVER:  3,
+	GAME_STATE_WIN:  4,
 
 	/**************地图块*****************/
-	WALL = 1;
-	GRID = 2;
-	GRASS = 3;
-	WATER = 4;
-	ICE = 5;
-	HOME = 9;
-	ANOTHREHOME = 8;
+	WALL:  1,
+	GRID:  2,
+	GRASS:  3,
+	WATER:  4,
+	ICE:  5,
+	HOME:  9,
+	ANOTHREHOME:  8,
 
 	/**************坦克及子弹的四个方向*****************/
-	UP = 0;
-	DOWN = 1;
-	LEFT = 2;
-	RIGHT = 3;
+	UP:  0,
+	DOWN:  1,
+	LEFT:  2,
+	RIGHT:  3,
 
 	/**************坦克及子弹的四个方向*****************/
-	ENEMY_LOCATION = [192, 0, 384]; // 相对与主游戏区
+	ENEMY_LOCATION:  [192, 0, 384], // 相对与主游戏区
 
 	/**************子弹类型*****************/
-	BULLET_TYPE_PLAYER = 1;
-	BULLET_TYPE_ENEMY = 2;
+	BULLET_TYPE_PLAYER:  1,
+	BULLET_TYPE_ENEMY:  2,
 	/**************爆炸类型****************/
-	CRACK_TYPE_TANK = 'tank';
-	CRACK_TYPE_BULLET = 'bullet';
-}
+	CRACK_TYPE_TANK:  'tank',
+	CRACK_TYPE_BULLET:  'bullet'
+};
--- src/app/canvas/tank/menu.component.ts
+++ src/app/canvas/tank/menu.component.ts
@@ -1,13 +1,54 @@
-import {Constant} from './constant.component.ts';
+import { Constant } from './constant.component';
 
 export class Menu {
 
-	ctx: CanvasRenderingContext2D,
-	x: number,
-	y: 
+	ctx: CanvasRenderingContext2D;
+	x: number;
+	y: number;
+	selectTank: Selectank;
+	playerNum: number;
+	times: number;
 
-	constructor() {
+	constructor(ctx: CanvasRenderingContext2D) {
 
-    }
+		this.ctx = ctx;
+		this.x = 0;
+		this.y = Constant.SCREEN_HEIGHT;
+		this.selectTank = new this.selectTank();
+		this.playerNum = 1;
+		this.times = 0;
+	}
+
+	/**
+	 * 画菜单
+	 */
+	draw() {
+		this.times ++;
+		let temp = 0;
+		if (Math.trunc(this.times / 6) % 2 === 0) {
+			temp = 0;
+		}else {
+			temp = this.selectTank.size;
+		}
+		this.y = this.y > 0 ? this.y -= 5 : 0;
+
+		this.ctx.clearRect(0, 0, Constant.SCREEN_WIDTH, Constant.SCREEN_HEIGHT);
+		this.ctx.save();
+
+		Constant.MENU_IMAGE.src = 'images/menu.gif';
+		this.ctx.drawImage(Constant.MENU_IMAGE, this.x, this.y);
+
+		Constant.RESOURCE_IMAGE.src = 'images/tankAll.gif';
+		this.ctx.drawImage(Constant.RESOURCE_IMAGE, Constant.POS.selectTank[0], Constant.POS.selectTank[0], this.selectTank.size, this.selectTank.size, this.selectTank.x, this.y + this.selectTank.ys[this.playerNum - 1], this.selectTank.size, this.selectTank.size);
+		this.ctx.restore();
+	}
+
+	/**
+	 * 选择玩家人数
+	 */
+	next(n) {
+		this.playerNum += n;
+		this.playerNum = this.playerNum > 2 ? 1 : this.playerNum < 1 ? 2 : this.playerNum;
+	}
 
 }
