468e342ed9bc3933cc0d86e820080abfa3f74edc
CuteBlackCat <854488571@qq.com>
2018-03-31 21:30:39 +0800

    add map

--- src/app/canvas/tank/map.ts
+++ src/app/canvas/tank/map.ts
@@ -1,4 +1,6 @@
 import { Num } from './num';
+import { Maps } from './mapData';
+import { Constant } from './constant';
 
 export class Map {
 
@@ -7,7 +9,7 @@ export class Map {
 	wallCtx: CanvasRenderingContext2D;
 	grassCtx: CanvasRenderingContext2D;
 
-	offserX: number; // 主游戏区的X轴的偏移量
+	offsetX: number; // 主游戏区的X轴的偏移量
 	offsetY: number; // 主游戏区的y轴的偏移量
 	wTileCount: number; // 主游戏区的宽度地图块数
 	hTileCount: number; // 主游戏区的高度地图块数
@@ -22,7 +24,7 @@ export class Map {
 		this.mapLevel = null;
 		this.wallCtx = wCtx;
 		this.grassCtx = gCts;
-		this.offserX = 32;
+		this.offsetX = 32;
 		this.offsetY = 16;
 		this.wTileCount = 26;
 		this.hTileCount = 26;
@@ -33,8 +35,59 @@ export class Map {
 		this.mapHeight = 416;
 	}
 
+	/**
+	 * 重置地图等级
+	 * @param level 地图等级
+	 */
 	setMapLevel(level: number) {
 		this.level = level;
-		const tempMap = 
+		const tempMap = `map${this.level}`;
+		const currentMap = Maps[tempMap];
+		this.mapLevel = [];
+
+		for (const map of currentMap) {
+			let i = 0;
+			this.mapLevel[i] = [];
+			for (const mapj of map) {
+				let j = 0;
+				this.mapLevel[i][j] = mapj;
+				j++;
+			}
+			i++;
+		}
+	}
+
+	/**
+	 * 绘制地图
+	 */
+	draw() {
+		this.wallCtx.fillStyle = '#7f7f7f';
+		this.wallCtx.fillRect(0, 0, Constant.SCREEN_WIDTH, Constant.SCREEN_HEIGHT);
+		this.wallCtx.fillStyle = '#000';
+		this.wallCtx.fillRect(this.offsetX, this.offsetY, this.mapWidth, this.mapHeight);
+
+		this.grassCtx.clearRect(0, 0, Constant.SCREEN_WIDTH, Constant.SCREEN_HEIGHT);
+
+		for (let i = 0; i < this.hTileCount; i++) {
+			for (let j = 0; j < this.wTileCount; j++) {
+				if (this.mapLevel[i][j] === Constant.WALL || this.mapLevel[i][j] === Constant.GRID || this.mapLevel[i][j] === Constant.WATER || this.mapLevel[i][j] === Constant.ICE) {
+					this.wallCtx.drawImage(Constant.RESOURCE_IMAGE, this.tileSize * (this.mapLevel[i][j] - 1) + Constant.POS['map'][0], Constant.POS['map'][1], this.tileSize, this.tileSize, j * this.tileSize + this.offsetX, i * this.tileSize + this.offsetY, this.tileSize, this.tileSize);
+				} else if (this.mapLevel[i][j] === Constant.GRASS) {
+					this.grassCtx.drawImage(Constant.RESOURCE_IMAGE, this.tileSize * (this.mapLevel[i][j] - 1) + Constant.POS['map'][0], Constant.POS['map'][1], this.tileSize, this.tileSize, j * this.tileSize + this.offsetX, i * this.tileSize + this.offsetY, this.tileSize, this.tileSize);
+				} else if (this.mapLevel[i][j] === Constant.HOME) {
+					this.wallCtx.drawImage(Constant.RESOURCE_IMAGE, Constant.POS['home'][0], Constant.POS['home'][1], this.homeSize, this.homeSize, j * this.tileSize + this.offsetX, i * this.tileSize + this.offsetY, this.homeSize, this.homeSize);
+				}
+			}
+		}
+
+		this.drawNoChange();
+		this.drawEnemyNum(maxEnemy);
+		this.drawLevel();
+		this.drawLives(0, 1);
+		this.drawLives(0, 2);
+	}
+
+	drawNoChange() {
+		this.wallCtx.drawImage
 	}
 }
