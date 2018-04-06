4f27284b19ab410715166f2a4a750034032d39bc
CuteBlackCat <854488571@qq.com>
2018-04-06 21:19:46 +0800

    story card

--- src/app/directive/directive.module.ts
+++ src/app/directive/directive.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SliderComponent } from './slider/slider.component';
 import { NgModule } from '@angular/core';
 import { CardComponent } from './card/card.component';
+import { StoryCardComponent } from './storycard/storycard.component';
 
 @NgModule({
 	declarations: [
 		CardComponent,
-		SliderComponent
+		SliderComponent,
+		StoryCardComponent
 	],
 	imports: [BrowserModule],
-	exports: [SliderComponent, CardComponent]
+	exports: [SliderComponent, CardComponent, StoryCardComponent]
 })
 
 export class FsDirectiveModule {
---
+++ src/app/directive/storycard/storycard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'fs-story-card',
+	templateUrl: './storycard.component.html',
+	styleUrls: ['./storycard.component.css']
+})
+
+export class StoryCardComponent {
+
+}
